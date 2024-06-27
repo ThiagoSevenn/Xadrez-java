7ad2af109bf5f76225b49b4c59e5cdfcd05ac341
Thiago Neves <thiagonevessc@gmail.com>
2024-06-27 19:45:06 -0300

    Criação da movimentação das peças

--- src/App.java
+++ src/App.java
@@ -1,11 +1,28 @@
+import java.util.Scanner;
+
 import boardgame.Board;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 
 public class App {
     public static void main(String[] args) throws Exception {
         ChessMatch chessMatch = new ChessMatch();
-        Interface.printBoard(chessMatch.getPieces());
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            Interface.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = Interface.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = Interface.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
+        
     }
 }
 
