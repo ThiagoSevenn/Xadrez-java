fb5d314be70ee2299ec0eb1f495182ec23e17fb0
ThiagoSevenn <thiagonevessc@gmail.com>
2026-02-03 11:28:04 -0300

    Jogada Especial Castling - Parte 2

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -91,6 +91,24 @@ public class ChessMatch {
             piecesOnTheBoard.remove(capturedPiece);
             capturedPieces.add(capturedPiece);
         }
+
+        // Castling : Movimento Especeial // Lado do Rei
+        if (p instanceof King && target.getColumn() == source.getColumn() + 2){
+            Position sourceT = new Position(source.getRow(),source.getColumn() + 3);
+            Position targetT = new Position(source.getRow(),source.getColumn() + 1);
+            ChessPiece rook = (ChessPiece) board.removePiece(sourceT);
+            board.placePiece(rook, targetT);
+            rook.increaseMoveCount();
+        }
+
+        // Castling : Movimento Especeial // Lado da Rainha
+        if (p instanceof King && target.getColumn() == source.getColumn() - 2){
+            Position sourceT = new Position(source.getRow(),source.getColumn() - 4);
+            Position targetT = new Position(source.getRow(),source.getColumn() - 1);
+            ChessPiece rook = (ChessPiece) board.removePiece(sourceT);
+            board.placePiece(rook, targetT);
+            rook.increaseMoveCount();
+        }
         return capturedPiece;
     }
 
@@ -103,6 +121,24 @@ public class ChessMatch {
             board.placePiece(capturedPiece, target);
             capturedPieces.remove(capturedPiece);
             piecesOnTheBoard.add(capturedPiece);
+        }
+
+        // Castling : Movimento Especeial // Lado do Rei
+        if (p instanceof King && target.getColumn() == source.getColumn() + 2){
+            Position sourceT = new Position(source.getRow(),source.getColumn() + 3);
+            Position targetT = new Position(source.getRow(),source.getColumn() + 1);
+            ChessPiece rook = (ChessPiece) board.removePiece(targetT);
+            board.placePiece(rook, sourceT);
+            rook.decreaseMoveCount();
+        }
+
+        // Castling : Movimento Especeial // Lado da Rainha
+        if (p instanceof King && target.getColumn() == source.getColumn() - 2){
+            Position sourceT = new Position(source.getRow(),source.getColumn() - 4);
+            Position targetT = new Position(source.getRow(),source.getColumn() - 1);
+            ChessPiece rook = (ChessPiece) board.removePiece(targetT);
+            board.placePiece(rook, sourceT);
+            rook.decreaseMoveCount();
         }
     }
 
